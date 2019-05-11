dae45e47608b8a1741edbef7afc84c743b8f741d
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2019-05-12 00:57:18 +0530

    Convert image to grayscale
    Convert image to grayscale for faster rendering as it needs only 1 channel for each pixel ranging from 0 to 255.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Self Driving Car using Deep Learning
+
+Requirements
+[+] IDE (Atom, Notepad++, Visual Studio Code etc)
+[+] Anaconda Navigator
+[+] numpy
+[+] openCV (Open Source Computer Vision Library)
+
+
+Steps Taken
+[+] Load image
+[+] Render image
+[+] Change image to grayscale
--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # imread() and imshow() functions will be used
 # imread() - used to load image
@@ -7,8 +8,14 @@ import cv2
 # read image from file and return as multidimensional numpy array containing relative intensities of each pixel in the image
 image = cv2.imread('test_image.jpg')
 
+# make a copy of image to work upon
+lane_image = np.copy(image)
+
+# convert image to grayscale. cvtColor is used to convert image to different color scales
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
 # after loading image, we have to render the image using imshow() function
-cv2.imshow('result', image)
+cv2.imshow('result', gray)
 
 # retain image for specified amount of time
 cv2.waitKey(0)
