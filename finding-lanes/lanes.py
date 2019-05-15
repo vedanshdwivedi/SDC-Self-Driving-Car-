fda62bdf4a09062ff870f811d5351627f5c6e1e0
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2019-05-16 00:53:50 +0530

    Optimizing Detection

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -3,6 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
 # imread() and imshow() functions will be used
 # imread() - used to load image
 # imshow() - renders the image
@@ -29,8 +37,7 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10) # arg4 is BGR color (blue) #arg5 is line thickness
     return line_image
 
@@ -50,6 +57,23 @@ def region_of_interest(image):
     return masked_image
 
 
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
 
 
 # read image from file and return as multidimensional numpy array containing relative intensities of each pixel in the image
@@ -58,9 +82,9 @@ image = cv2.imread('test_image.jpg')
 # make a copy of image to work upon
 lane_image = np.copy(image)
 
-canny = canny(lane_image)
+canny_image = canny(lane_image)
 
-cropped_image = region_of_interest(canny)
+cropped_image = region_of_interest(canny_image)
 
 # to identify edges using Hough Transform (refer README), p = x cosƟ + y sinƟ
 # arg1 - Image
@@ -73,8 +97,8 @@ cropped_image = region_of_interest(canny)
 # OPTIMIZE: use hit and trial method to find arg4
 # arg2 and arg3 define the size of bins, larger the bins, less is the precision
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-
-line_image = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 
 # Multiply lane_image with 0.8 to make it darker, line_image has 20% more weight so it will be more visible
 # arg5 is gamma component
